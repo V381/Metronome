59e88990b92fe971202e2b96ceef2de9271e9908
Pavle <paavlee@yahoo.com>
2014-08-31 11:45:18 +0200

    Fixed some bugs

--- js.js
+++ js.js
@@ -1,43 +1,50 @@
 /**
  * Created by JS on 8/2/14.
  */
-var minute = 60000,
-    bmp = 40,
-    button = document.querySelector(".add"),
-    input1 = document.querySelector(".txt1"),
-    button2 = document.querySelector(".stop");
+(function(){
+    var minute = 60000,
+        bmp = 40,
+        button = document.querySelector(".add"),
+        input1 = document.querySelector(".txt1"),
+        button2 = document.querySelector(".stop");
 
-var audio = document.querySelector("audio");
+    var audio = document.querySelector("audio");
 
 
-button.addEventListener("click", function(e){
-    if (e.target){
-        sarko();
-        this.setAttribute("disabled", "disabled");
+    button.addEventListener("click", function(e){
+
+
+        if (e.target){
+            sarko();
+            e.target.setAttribute("disabled", "disabled");
+        }
+
+
+    },false);
+
+
+    button2.addEventListener("click", function(){
+        audio.pause();
+        audio.currentTime = 0;
+        var id = window.setTimeout(function() {}, 0);
+        while (id--) {
+            window.clearTimeout(id);
+        }
+        button.removeAttribute("disabled");
+
+    }, false);
+
+    function sarko(){
+        console.log(setTimeout(function(){
+            sarko();
+        }, minute/input1.value));
+        audio.load();
+        audio.play();
     }
-},false);
 
-button2.addEventListener("keydown", function(e){
-    if (e.target){
-        sarko();
+    function stop(){
+        clearTimeout(sarko());
+        audio.pause();
+        audio.currentTime = 0;
     }
-},false);
-
-
-
-
-
-
-button2.addEventListener("click", function(){
-    clearTimeout(sarko());
-
-}, false);
-
-function sarko(){
-    console.log(setTimeout(function(){
-        sarko();
-    }, minute/input1.value));
-    audio.load();
-    audio.play();
-}
-
+})();
